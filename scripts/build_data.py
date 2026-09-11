30e25d9bf4702ccf4ac9bff4d5a7d6bd2b24cb0f
RodrigoManassero <rodrigomanassero@hotmail.com>
2026-09-11 19:37:27 -0300

    Remove fetch_cammesa.py from data fetching

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -114,7 +114,6 @@ def main():
     errors += run('fetch_enargas.py')
     errors += run('fetch_enargas_ing.py')
     errors += run('fetch_enargas_ps.py')
-    errors += run('fetch_cammesa.py')
     errors += run('fetch_cammesa_ppo.py')
     errors += run('fetch_cammesa_redespacho.py')
     errors += run('fetch_cammesa_weekly.py')
